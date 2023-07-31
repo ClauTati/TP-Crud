e524248c57218db2ea3843938a6dc2098db3dcef
ClauTati <122483506+ClauTati@users.noreply.github.com>
2023-07-31 15:57:53 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+document.getElementById("header").innerHTML=` <nav class="navbar navbar-expand-sm
+navbar-light" style="background-color: #c1ea63;">
+<div class="container" >
+<a class="navbar-brand"  href="index.html" style="font-size:40px;">Inicio </a>
+<button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse"
+data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
+</button>
+
+
+
+
+</div>
+
+</nav>
+`
